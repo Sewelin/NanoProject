5c1bfdc97d387daae5b60750f7e66a31fa030cb4
Apoal <drouillard.damien@gmail.com>
2019-12-01 23:38:45 +0100

    :zap: [ADD] Wwise Place Holders + Tests sounds for integration tests

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,8 +13,25 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID MUSIC_TEST2D = 4188790071U;
-        static const AkUniqueID SFX_SWORD_TEST3D = 665833074U;
+        static const AkUniqueID BACKWARD_JUMP = 3686462113U;
+        static const AkUniqueID DASH = 1942692385U;
+        static const AkUniqueID DASH_STOP = 3615040804U;
+        static const AkUniqueID MENU_ATMOS = 789497435U;
+        static const AkUniqueID MENU_BACK = 3063554414U;
+        static const AkUniqueID MENU_CHANGE_OPTION = 2340917041U;
+        static const AkUniqueID MENU_ENTER = 1952436935U;
+        static const AkUniqueID MENU_PAUSE = 2170009975U;
+        static const AkUniqueID MENU_PLAY = 105544633U;
+        static const AkUniqueID MENU_QUIT = 2776193354U;
+        static const AkUniqueID MENU_UPDOWN = 2587230380U;
+        static const AkUniqueID PLAYER_ONE_BACKWARD = 1183016477U;
+        static const AkUniqueID PLAYER_ONE_BOWING = 375910062U;
+        static const AkUniqueID PLAYER_ONE_FORWARD = 972262685U;
+        static const AkUniqueID PLAYER_TWO_BACKWARD = 2507762279U;
+        static const AkUniqueID PLAYER_TWO_BOWING = 2367140880U;
+        static const AkUniqueID PLAYER_TWO_FORWARD = 184927275U;
+        static const AkUniqueID SWORD_HIT = 4057287994U;
+        static const AkUniqueID VERTICAL_ATTACK = 2665482738U;
     } // namespace EVENTS
 
     namespace BANKS
@@ -25,8 +42,34 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID AMBIENT = 77978275U;
+        static const AkUniqueID ATMOS = 3034623661U;
+        static const AkUniqueID DEATH = 779278001U;
+        static const AkUniqueID M_MENU = 2338369940U;
+        static const AkUniqueID M_PLAY = 1992326939U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MENU = 2607556080U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID OBJECTS = 1695690031U;
+        static const AkUniqueID OPTIONS = 4141130937U;
+        static const AkUniqueID P_ONE = 1123360410U;
+        static const AkUniqueID P_ONE_FOL = 2275604156U;
+        static const AkUniqueID P_ONE_SFX = 2126326874U;
+        static const AkUniqueID P_TWO = 1242230844U;
+        static const AkUniqueID P_TWO_FOL = 4290589570U;
+        static const AkUniqueID P_TWO_SFX = 3599308920U;
+        static const AkUniqueID REVERBS = 3545700988U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID SWORD = 2454616260U;
+        static const AkUniqueID UI = 1551306167U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID REV_MENU = 2766901782U;
+        static const AkUniqueID REV_MENU_MUSIC = 2116471102U;
+        static const AkUniqueID REV_SCENE = 3548165401U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
