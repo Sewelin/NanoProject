ab152795e0981c253781522832f6775664d34ec8
Théo O <togomiti@hotmai.fr>
2019-12-11 11:51:46 +0100

    :hankey: Animator complete

--- Assets/Scripts/Controllers/States/DashState.cs
+++ Assets/Scripts/Controllers/States/DashState.cs
@@ -2,9 +2,12 @@
 
 public class DashState : AbstractAttackState
 {
+    private static readonly int AttDash = Animator.StringToHash("AttDash");
+
     public DashState(GameManager gameManager, AbstractController controller, int dir) :
         base(gameManager, controller, gameManager.dashAttackParameters, dir)
     {
+        controller.characterInfo.Animator.SetTrigger(AttDash);
     }
     
     public override ControllerStateName Name()
