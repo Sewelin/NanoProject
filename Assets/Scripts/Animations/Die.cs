2f9ee31784198bb4dd3354d2bfd928a9a727cb27
Becher <mbelkhenchir@gmail.com>
2019-12-12 18:22:38 +0100

    fix movable corps

--- Assets/Scripts/Animations/Die.cs
+++ Assets/Scripts/Animations/Die.cs
@@ -13,6 +13,7 @@ public class Die : AbstractAnimation
         transform.position += new Vector3(0,0,0.5f);
         transform.GetComponentInChildren<Animator>().SetTrigger(DieAnim);
         controller.characterInfo.characterAssigned = false;
+
         //TODO Suppr visual effect
         //GetComponent<Renderer>().material.color = Color.black;
     }
