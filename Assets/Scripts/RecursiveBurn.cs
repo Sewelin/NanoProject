b32be1fb570ad762ab2e27532a2aa11e81bf2eeb
Becher <mbelkhenchir@gmail.com>
2019-12-14 11:44:45 +0100

    Vous n'avez pas de branche vous vivez dans des ponts et vous commit dans des vlops

---
+++ Assets/Scripts/RecursiveBurn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecursiveBurn : MonoBehaviour
+{
+    float _progress = 0;
+    Material shader;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
